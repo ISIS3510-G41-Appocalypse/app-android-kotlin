e3eedd20ad7d16799a062536ef0378da512ff7d7
LuigiEnLaCasa <l.pinillag@uniandes.edu.co>
2026-03-19 10:17:35 -0500

    Welcome Screen V1. completed

--- app/src/main/java/com/gn41/appandroidkotlin/presentation/views/WelcomeScreen.kt
+++ app/src/main/java/com/gn41/appandroidkotlin/presentation/views/WelcomeScreen.kt
@@ -1,18 +1,32 @@
 package com.gn41.appandroidkotlin.presentation.views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WelcomeScreen(onLoginClick: () -> Unit, onRegisterClick: () -> Unit){
-    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween){
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colorScheme.background)
+        .statusBarsPadding()
+        .navigationBarsPadding()
+        .padding(horizontal = 24.dp, vertical = 24.dp), verticalArrangement = Arrangement.SpaceBetween){
+
 
         Column(
             modifier = Modifier.weight(1f),
@@ -21,29 +35,52 @@ fun WelcomeScreen(onLoginClick: () -> Unit, onRegisterClick: () -> Unit){
 
             WelcomeHeader()
             WelcomeMessage()
+            WelcomeButtons(onLoginClick = onLoginClick, onRegisterClick = onRegisterClick)
+
 
         }
-
-
-        WelcomeButtons(onLoginClick = onLoginClick, onRegisterClick = onRegisterClick)
-
-
     }
 }
 
 
 @Composable
 fun WelcomeHeader(){
-    Text(text = "Happy Ride", color = MaterialTheme.colorScheme.primary)
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+    Text(text = "[Poner el logo]" , style = MaterialTheme.typography.titleLarge , color = MaterialTheme.colorScheme.tertiary)
+    Text(text = "Happy Ride", style = MaterialTheme.typography.titleLarge , color = MaterialTheme.colorScheme.onBackground)
 
+    Spacer(modifier = Modifier.height(4.dp))
+    Text(text = "Universidad de los Andes", style = MaterialTheme.typography.bodyLarge, color = MaterialTheme.colorScheme.primary)
+
+    }
 }
 
 @Composable
-fun WelcomeMessage(){}
+fun WelcomeMessage(){
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text(text = "Muévete fácil\ndentro de", style = MaterialTheme.typography.titleLarge, textAlign = TextAlign.Center, color = MaterialTheme.colorScheme.onSurface)
+        Text(text = "Uniandes", style = MaterialTheme.typography.titleLarge, textAlign = TextAlign.Center, color = MaterialTheme.colorScheme.primary)
+        Spacer(modifier = Modifier.height(height = 16.dp))
+
+        Text(text = "Comparte wheels de forma simple y sin complicaciones.", style = MaterialTheme.typography.bodyLarge, textAlign = TextAlign.Center, color = MaterialTheme.colorScheme.onSurface)
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(text = "Your ride. No stress.", style = MaterialTheme.typography.bodyLarge, textAlign = TextAlign.Center,color = MaterialTheme.colorScheme.onSurface)
+
+
+
+    }
+
+}
 
 
 @Composable
 fun WelcomeButtons(onLoginClick: () -> Unit, onRegisterClick: () -> Unit){
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
     Button(onClick = onRegisterClick){ Text(text = "Registrarse")}
     Button(onClick = onLoginClick) {Text(text = "¿Ya tienes una cuenta? Inicia Sesión")}
+    }
 }
